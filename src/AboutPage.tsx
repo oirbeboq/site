aa5ef4671b19c2b30d009b97c79733368fc98d90
oirbeboq <130990969+oirbeboq@users.noreply.github.com>
2026-06-17 19:20:17 +0200

    about me page updated

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -5,11 +5,41 @@ export default function AboutPage() {
             <div
                 className="profile-image">
                 <img src="/about-page/profile.png"/>
+                <p>Randy Addaé Manu</p>
             </div>
             <div 
-                className="bio">
+                className="bio-container">
                    <p>my name is animation and i make randy make games.</p> 
-                </div>
+
+            </div>
+            <div 
+                className="contact-container">
+                    <a href="mailto:oirbeboq@gmail.com">Email <span>[-]</span></a>
+                    {/*
+                    <a href="https://www.instagram.com/oirbeboq/">Instagram <span>[-]</span></a>
+                    <a href="https://www.youtube.com/@byoirt">Youtube <span>[-]</span></a>
+                    */}
+            </div>
+            <div
+                className="tools-container">
+                    <h1> Tools</h1>
+                    <ul>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe After Effects</li>
+                        <li>Adobe Indesign</li>
+                        <li>Aseprite</li>
+                        <li>Blender</li>
+                        <li>Clip Studio Paint</li>
+                        <li>Unity</li>
+                        <li>Unreal Engine</li>
+                        <li>Procreate</li>
+                        <li>Toon Boom Harmony</li>
+                        
+
+                        
+                    </ul>
+            </div>
+
     </div>
    
 
